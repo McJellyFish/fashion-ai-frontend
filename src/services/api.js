a35a100ad22cc1b36b67e402e7d965d43d30ef84
McJellyFish <104248200+McJellyFish@users.noreply.github.com>
2026-02-21 18:58:30 -0500

    Fixed double /analyze endpoint bug

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mcjellyfish-fashion-ai-backend.hf.space/analyze"
+  baseURL: "https://mcjellyfish-fashion-ai-backend.hf.space"
 });
 
 export const analyzeImage = async (imageFile) => {
